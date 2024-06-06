6fd98e8f050372da272306f38f39c05ed8d83663
ericknovais <erick.hora@outlook.com>
2024-06-05 23:10:51 -0300

    Validando Campos

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -1,14 +1,28 @@
 window.onload = function() 
 {
-    
+    document.getElementById("txtPeso").addEventListener("change", function(){
+        this.value = parseFloat(this.value).toFixed(2);
+    });
+    document.getElementById("txtAltura").addEventListener("change", function(){
+        this.value = parseFloat(this.value).toFixed(2);
+    });
 };
 
 function CalculaIMC() {
     var peso = document.getElementById("txtPeso")
     var altura = document.getElementById("txtAltura")
+    alert(peso.value)
     if(peso.value == '' || altura.value == '')
-        return
+        return false;
+
+    if(peso.value < 0 || altura.value < 0)
+    {
+        alert("valores não podem ser negativos")
+        return false;
+    }
     
-    alert('Peso:' + peso.value)
-    alert('Altura:' + altura.value)
+    alert(parseFloat(peso.value))
+    // alert('Peso:' + peso.value)
+    // alert('Altura:' + altura.value)
+    let imc = parseInt()
   }
