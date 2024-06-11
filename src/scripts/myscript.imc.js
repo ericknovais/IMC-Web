9de49857bb5951a40798b3c25f441da6d8e39b2e
ericknovais <erick.hora@outlook.com>
2024-06-10 22:54:04 -0300

    Refinamento do css e removendo o alert do imc e deixando um texto em tela,

--- src/scripts/myscript.imc.js
+++ src/scripts/myscript.imc.js
@@ -6,37 +6,61 @@ function CalculaIMC() {
     let peso = PegaValorDoCampo("txtPeso");
     let altura = PegaValorDoCampo("txtAltura");
     if(ValidaCampos(peso, altura)){
-        let imc = parseFloat(parseFloat(peso) / parseFloat(altura **2)).toFixed(2); 
+        let imc = parseFloat(peso / (altura **2)).toFixed(2); 
         MostraIMC(imc);
     }
+    CancelaSubmitDoForm();
 };
 
 function MostraIMC(imc)
 {
+    var textoIMC = '';
     if(imc < 18.5 )
-        alert("Magreza, IMC = "+imc);
+        textoIMC ="Magreza";
     else if (imc < 24.9 )
-        alert("Normal, IMC = "+imc);
+        textoIMC = "Normal";
     else if (imc < 30 )
-        alert("Sobrepeso, IMC = "+imc);
+        textoIMC = "Sobrepeso";
     else 
-        alert("Obesidade, IMC = "+imc);
-             
+        textoIMC ="Obesidade";
+    
+    SetValorCampo('lblIMC',  `Seu IMC: ${textoIMC}, ${imc}`, ehInnerHTML = true);
 };
 
 function ValidaCampos(peso, altura)
 {
     if(peso === '' || altura === '')
-        return;
+        return false;
 
     if(peso < 0 || altura < 0)
     {
         alert("valores não podem ser negativos")
-        return;
+        return false;
     }
     return true;
 }
 
 function PegaValorDoCampo(campo){
     return document.getElementById(campo).value.replace('.','').replace(',','.');
+}
+
+function Limpar(){
+    SetValorCampo('txtPeso','');
+    SetValorCampo('txtAltura','');
+    SetValorCampo('lblIMC', '', ehInnerHTML = true)
+}
+
+function SetValorCampo(campo, valor, ehInnerHTML = false){
+    
+    if(ehInnerHTML)
+        document.getElementById(campo).innerHTML = valor;     
+    else
+        document.getElementById(campo).value = valor;
+}
+
+function CancelaSubmitDoForm(){
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+    });
 }
